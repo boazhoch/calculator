481a2aceb8c8df116054921b41037f66da0a1a89
Boaz Hoch <boazmier@gmail.com>
2019-03-10 21:31:11 +0200

    init

--- src/Components/Btn/Btn.tsx
+++ src/Components/Btn/Btn.tsx
@@ -1,26 +1,10 @@
 import React, { Component } from "react";
-import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+interface IProps {
+  onClick(value: string): void;
+ }
 
-export default App;
+const Btn = (props: IProps) => <button onClick={props.onClick}></button>
+
+
+export default Btn;
